cbc1b500ecc56d1aab0ccc84270ef528850350f9
Umutcan-YEL <umutcanyel132@gmail.com>
2023-11-11 17:47:35 +0300

    Tarih ve Dil Güncellemesi

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -11,6 +11,7 @@ import ButtonGroup from "@mui/material/ButtonGroup";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import FormHelperText from "@mui/material/FormHelperText";
+import Moment from "moment";
 
 const WeatherApp = () => {
   const [weatherData, setWeatherData] = useState(null);
@@ -20,6 +21,29 @@ const WeatherApp = () => {
   const [city, setCity] = useState();
   const [language, setLanguage] = useState("tr");
   const [number, setNumber] = useState(5);
+
+  const changeDaylang = (day) => {
+    if (language === "tr") {
+      switch (day) {
+        case "Monday":
+          return "Pazartesi";
+        case "Tuesday":
+          return "Salı";
+        case "Wednesday":
+          return "Çarşamba";
+        case "Thursday":
+          return "Perşembe";
+        case "Friday":
+          return "Cuma";
+        case "Saturday":
+          return "Cumartesi";
+        case "Sunday":
+          return "Pazar";
+      }
+    } else {
+      return day;
+    }
+  };
 
   const handleSearch = (event) => {
     setSearchQuery(event);
@@ -91,182 +115,192 @@ const WeatherApp = () => {
   }
 
   return (
-    <div className="Weather">
-      <br />
-      <Container fluid>
-        <Row>
-          <Col>
-            <TextField
-              id="outlined-basic"
-              label={ctn}
-              variant="filled"
-              onChange={(e) => handleSearch(e.target.value)}
-              onKeyDown={handleEnter}
-              autoComplete="off"
-            />
-            <br />
-            <br />
-          </Col>
+    <body>
+      <div className="Weather">
+        <br />
+        <Container fluid>
+          <Row>
+            <Col>
+              <TextField
+                id="outlined-basic"
+                label={ctn}
+                variant="filled"
+                onChange={(e) => handleSearch(e.target.value)}
+                onKeyDown={handleEnter}
+                autoComplete="off"
+              />
+              <br />
+              <br />
+            </Col>
 
-          <Col md={9}>
-            <h2 className="text-center">
-              {cw} <p style={{ color: "red" }}>{weatherData.city.name}</p>{" "}
-            </h2>
-            <Table
-              striped
-              bordered
-              responsive
-              size="sm"
-              className="text-center"
-              variant="dark"
-            >
-              <thead>
-                <tr>
-                  <th>{hour}</th>
-                  <th>{description}</th>
-                  <th></th>
-                  <th>{temp}</th>
-                  <th>{feels}</th>
-                  <th>{humidity}</th>
-                  <th>{wind}</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr key={weatherData.list[0].dt}>
-                  <td>
-                    <br />
-                    {weatherData.list[0].dt_txt.slice(0, 16)}
-                  </td>
-                  <td>
-                    <br />
-                    {weatherData.list[0].weather[0].description}
-                  </td>
-                  <td>
-                    {" "}
-                    <img
-                      src={`https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`}
-                      alt="icon"
-                    />
-                  </td>
-                  <td>
-                    <br />
-                    {weatherData.list[0].main.temp}°C
-                  </td>
-                  <td>
-                    <br />
-                    {weatherData.list[0].main.feels_like}°C
-                  </td>
-                  <td>
-                    <br />%{weatherData.list[0].main.humidity}
-                  </td>
-                  <td>
-                    <br />
-                    {weatherData.list[0].wind.speed}km/h
-                  </td>
-                </tr>
-              </tbody>
-            </Table>
-          </Col>
-          <Col>
-            <ButtonGroup
-              style={{ marginLeft: "2rem" }}
-              variant="text"
-              aria-label="text button group"
-            >
-              <Button onClick={() => changeLang("tr")}>Türkçe</Button>
-              <Button onClick={() => changeLang("en")}>English</Button>
-            </ButtonGroup>
-            <Select
-              sx={{ mt: 2, ml: 6, minWidth: 120 }}
-              size="small"
-              label={number}
-              value={number}
-              variant="filled"
-              onChange={handleChange}
-            >
-              <MenuItem value={5}>5</MenuItem>
-              <MenuItem value={10}>10</MenuItem>
-              <MenuItem value={20}>20</MenuItem>
-              <MenuItem value={30}>50</MenuItem>
-              <MenuItem value={50}>50</MenuItem>
-            </Select>
-            <FormHelperText sx={{ ml: 6, minWidth: 120 }}>
-              Getirelecek Saat Sayısı
-            </FormHelperText>
-          </Col>
-        </Row>
-      </Container>
-
-      <Container fluid>
-        <Table
-          striped
-          bordered
-          responsive
-          size="sm"
-          className="text-center"
-          variant="dark"
-        >
-          <thead>
-            <tr>
-              <th>{hour}</th>
-              <th>{description}</th>
-              <th></th>
-              <th>{temp}</th>
-              <th>{feels}</th>
-              <th>{humidity}</th>
-              <th>{wind}</th>
-            </tr>
-          </thead>
-          <tbody>
-            {weatherData.list.slice(1, 100).map((hours, index) => {
-              return (
-                index < number && (
-                  <tr key={hours.dt}>
+            <Col md={9}>
+              <h2 className="text-center">
+                {cw} <p style={{ color: "red" }}>{weatherData.city.name}</p>{" "}
+              </h2>
+              <Table
+                striped
+                bordered
+                responsive
+                size="sm"
+                className="text-center"
+                // variant="dark"
+              >
+                <thead>
+                  <tr>
+                    <th>{hour}</th>
+                    <th>{description}</th>
+                    <th></th>
+                    <th>{temp}</th>
+                    <th>{feels}</th>
+                    <th>{humidity}</th>
+                    <th>{wind}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr key={weatherData.list[0].dt}>
                     <td>
-                      {" "}
                       <br />
-                      {hours.dt_txt.slice(0, 16)}
+                      {changeDaylang(
+                        Moment(weatherData.list[0].dt_txt.slice(0, 10)).format(
+                          "dddd"
+                        )
+                      )}
+                      {weatherData.list[0].dt_txt.slice(10, 16)}
                     </td>
                     <td>
-                      {" "}
                       <br />
-                      {hours.weather[0].description}
+                      {weatherData.list[0].weather[0].description}
                     </td>
                     <td>
                       {" "}
                       <img
-                        src={`https://openweathermap.org/img/wn/${hours.weather[0].icon}@2x.png`}
+                        src={`https://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`}
                         alt="icon"
-                        width={100}
                       />
                     </td>
                     <td>
-                      {" "}
                       <br />
-                      {hours.main.temp}°C
+                      {weatherData.list[0].main.temp}°C
                     </td>
                     <td>
-                      {" "}
                       <br />
-                      {hours.main.feels_like}°C
+                      {weatherData.list[0].main.feels_like}°C
                     </td>
                     <td>
-                      {" "}
-                      <br />%{hours.main.humidity}
+                      <br />%{weatherData.list[0].main.humidity}
                     </td>
                     <td>
-                      {" "}
                       <br />
-                      {hours.wind.speed}km/h
+                      {weatherData.list[0].wind.speed} km/h
                     </td>
                   </tr>
-                )
-              );
-            })}
-          </tbody>
-        </Table>
-      </Container>
-    </div>
+                </tbody>
+              </Table>
+            </Col>
+            <Col>
+              <ButtonGroup
+                style={{ marginLeft: "2rem" }}
+                variant="text"
+                aria-label="text button group"
+              >
+                <Button onClick={() => changeLang("tr")}>Türkçe</Button>
+                <Button onClick={() => changeLang("en")}>English</Button>
+              </ButtonGroup>
+              <Select
+                sx={{ mt: 2, ml: 6, minWidth: 120 }}
+                size="small"
+                label={number}
+                value={number}
+                variant="filled"
+                onChange={handleChange}
+              >
+                <MenuItem value={5}>5</MenuItem>
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={20}>20</MenuItem>
+                <MenuItem value={30}>50</MenuItem>
+                <MenuItem value={50}>50</MenuItem>
+              </Select>
+              <FormHelperText sx={{ ml: 6, minWidth: 120 }}>
+                Getirelecek Saat Sayısı
+              </FormHelperText>
+            </Col>
+          </Row>
+        </Container>
+
+        <Container fluid>
+          <Table
+            striped
+            bordered
+            responsive
+            size="sm"
+            className="text-center"
+            // variant="dark"
+          >
+            <thead>
+              <tr>
+                <th>{hour}</th>
+                <th>{description}</th>
+                <th></th>
+                <th>{temp}</th>
+                <th>{feels}</th>
+                <th>{humidity}</th>
+                <th>{wind}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {weatherData.list.slice(1, 100).map((hours, index) => {
+                return (
+                  index < number && (
+                    <tr key={hours.dt}>
+                      <td>
+                        {" "}
+                        <br />
+                        {changeDaylang(
+                          Moment(hours.dt_txt.slice(0, 10)).format("dddd")
+                        )}
+                        {hours.dt_txt.slice(10, 16)}
+                      </td>
+                      <td>
+                        {" "}
+                        <br />
+                        {hours.weather[0].description}
+                      </td>
+                      <td>
+                        {" "}
+                        <img
+                          src={`https://openweathermap.org/img/wn/${hours.weather[0].icon}@2x.png`}
+                          alt="icon"
+                          width={100}
+                        />
+                      </td>
+                      <td>
+                        {" "}
+                        <br />
+                        {hours.main.temp}°C
+                      </td>
+                      <td>
+                        {" "}
+                        <br />
+                        {hours.main.feels_like} °C
+                      </td>
+                      <td>
+                        {" "}
+                        <br />% {hours.main.humidity}
+                      </td>
+                      <td>
+                        {" "}
+                        <br />
+                        {hours.wind.speed} km/h
+                      </td>
+                    </tr>
+                  )
+                );
+              })}
+            </tbody>
+          </Table>
+        </Container>
+      </div>
+    </body>
   );
 };
 
