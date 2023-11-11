ad78ebaa78c7b8a71fc7dc8768ff13cdadd91ae5
Umutcan-YEL <umutcanyel132@gmail.com>
2023-11-11 18:48:00 +0300

    basic update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
-  base: "/WeatherApp/",
+  base: "./WeatherApp/",
   plugins: [
     react(),
     federation({
